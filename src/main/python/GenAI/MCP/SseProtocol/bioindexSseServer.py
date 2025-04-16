16dd5983517192feb143eb9b76a3bdce29f2ff85
Marc Duby <mduby@broadinstitute.org>
2025-04-16 11:47:16 -0400

    mcp: added a server using sse

--- src/main/python/GenAI/MCP/SseProtocol/bioindexSseServer.py
+++ src/main/python/GenAI/MCP/SseProtocol/bioindexSseServer.py
@@ -1,9 +1,16 @@
 
 # imports
 from mcp.server.fastmcp import FastMCP
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 # global vars
 mcp = FastMCP(name="Bioindex MCP Server")
+
 
 # methods
 @mcp.tool()
@@ -14,6 +21,10 @@ def greeting(hint: str) -> str:
     Args:
         hint: The hint for the type of greeting
     '''
+    # log
+    logger.info(f"=============== Received query for greeting")
+
+    # return
     return "Hello, myname is the Flannick Lab's remote MCP server"
 
 
@@ -22,4 +33,3 @@ if __name__ == "__main__":
     mcp.settings.port = 8081
     mcp.run(transport='sse')
 
-    
